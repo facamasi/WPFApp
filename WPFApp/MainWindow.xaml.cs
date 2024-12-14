ac3c3f421b4bab1629a22e894c4d075ab413e2f4
Fabian Camilo Marriaga Sierra <fabiancamilo15@msn.com>
2024-12-14 14:29:25 -0500

    Agregar archivos de proyecto.

---
+++ WPFApp/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        CreateTask();
+    }
+
+    void AddMessage(string message)
+    {
+        this.Dispatcher.Invoke(() =>
+        {
+            Messages.Content +=
+            $"Mensaje: {message}, " +
+            $"Hilo actual: {Thread.CurrentThread.ManagedThreadId}\n";
+        });
+    }
+
+
+    void CreateTask()
+    {
+        Task T;
+        Action Code = new Action(ShowMessage);
+        T = new Task(Code);
+
+        Task T2 = new Task(delegate
+        {
+            MessageBox.Show("Ejecutando una tarea en un método anónimo");
+        }
+        );
+
+        Task T3 = new Task(() => ShowMessage());
+
+        //Expresion Lambda
+        Task T4 = new Task(() => MessageBox.Show("Ejecutando la tarea 4"));
+
+        Task T5 = new Task(() =>
+        {
+            DateTime CurrentDate = DateTime.Today;
+            DateTime StartDate = CurrentDate.AddDays(30);
+            MessageBox.Show($"Tarea 5. Fecha Calculada: {StartDate}");
+        }
+        );
+
+        Task T6 = new Task((message) =>
+        MessageBox.Show(message.ToString()), "Expresion Lambda con parámentros");
+
+        Task T7 = new Task(() => AddMessage("Ejecutando la tarea"));
+        T7.Start();
+        AddMessage("En el hilo principal");
+
+    }
+    void ShowMessage()
+    {
+        MessageBox.Show("Ejecutando el metodo ShowMessage");
+    }
+
+}
